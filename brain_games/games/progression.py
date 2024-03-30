d6d63fadea7372030cee1233c068d93933cda56e
mtalipowa <talipovamilana@gmail.com>
2024-03-30 15:56:36 +0400

    Исправление по линтеру

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,15 +5,15 @@ rules = 'What number is missing in the progression?'
 
 def main():
     lenth = randint(5, 10)
-    first_number = randint (1, 5)
-    step = randint (1, 5)
-    last_number = first_number + step*(lenth)
+    first_number = randint(1, 5)
+    step = randint(1, 5)
+    last_number = first_number + step * (lenth)
 
     list = [*range(first_number, last_number, step)]
 
-    index = randint(0, lenth-1)
+    index = randint(0, lenth - 1)
     result = str(list[index])
-    
+
     list[index] = ".."
     list = [str(i) for i in list]
     question = " ".join(list)
